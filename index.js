ab6cfb4e8dd59224b9c61d594ef25445132799f1
jiacheng2018 <penggysosmart@gmail.com>
2019-11-30 19:25:25 +1000

    commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+// const passport = require('passport');
+// const googleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 app.get('/', (req, res) => {
           res.send({ hi: 'you are good' });
 });
+
 app.listen(5000);
